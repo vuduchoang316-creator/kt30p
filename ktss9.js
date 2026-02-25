d9de011b723e3ea03a69b5972d5ec282a890cd99
vuduchoang316-creator <vuduchoang316@gmail.com>
2026-02-25 14:20:58 +0700

    mup

--- ktss9.js
+++ ktss9.js
@@ -3,11 +3,15 @@ let revenues = [1500,2800,1200,-500,3200];
 let listOrder = [];
 let arr;
 for (let i = 0 ; i < orders.length ; i++){
-     arr= `${orders[i]}-${revenues}`;
+     arr= `${orders[i]}-${revenues[i]}`;
      listOrder.push(arr);
 }
 const orderReports = listOrder.map((item) => {
     return `${item.split("-")[0]} mang về ${item.split("-")[1]} USD`
 });
 console.log(orderReports);
-
+const total = revenues.filter(item => item > 0);
+const totalpass = total.reduce((arr,cur) =>{
+    return arr += cur
+});
+console.log(totalpass);
