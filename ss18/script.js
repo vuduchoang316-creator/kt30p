46e84a008ad659cf22596ac40824019d9d36946b
vuduchoang316-creator <vuduchoang316@gmail.com>
2026-03-18 13:45:00 +0700

    mup

--- ss18/script.js
+++ ss18/script.js
@@ -3,7 +3,7 @@ let inputPhone = document.getElementById("contact-phone");
 let inputEmail = document.getElementById("contact-email");
 let btn_add = document.getElementById("btn-add");
 let tBody = document.getElementById("contact-tbody");
-
+    localStorage.setItem("count",5);
 const addPerson = (e) => {
     e.preventDefault();
     let nameEl = inputName.value;
@@ -14,23 +14,32 @@ const addPerson = (e) => {
         alert("Tên không được để trống");
         return;
     }else if(nameEl.length < 2){
-        alert("Họ tên ít nhất phải tưef 2 kí tự");
+        alert("Họ tên ít nhất phải từ 2 kí tự");
+        return;
     }
 
     if(phoneEl ===""){
         alert("Số điện thoại không được để trống");
         return;
+    }else if(phoneEl[0] !== "0"){
+        alert("Ký tự đầu tiên phải bắt đầu bằng chữ số 0");
+        return;
+    }else if(phoneEl.trim().length !== 10){
+        alert("Không phải 10 chữ số");
+        return;
     }
     if(emailEl.trim() ===""){
        alert("Email không được để trống");
        return;
+    }else if(!emailEl.includes('@')){
+       alert("Email không đúng định dạng");
     }
 
     let tr = document.createElement("tr");
-    localStorage.setItem("count",5);
-let count = localStorage.getItem("count");
-let countS = +count + 1;
-localStorage.setItem("count",countS);
+
+    let count = localStorage.getItem("count");
+    let countS = +count + 1;
+    localStorage.setItem("count",countS);
     tr.innerHTML=`
          <td>${countS}</td>
          <td>${nameEl}</td>
---
+++ ss19/script.js
@@ -0,0 +1,43 @@
+let fullNameEl = document.getElementById("fullName");
+let inputNameEl = document.getElementById("inputName");
+let passWord = document.getElementById("pass");
+let rePassWord  = document.getElementById("repass");
+let btn = document.getElementById("btn-add")
+
+const addForm = () => {
+    let info = [];
+    let add = {
+        fullname:fullNameEl.value,
+        usersname:inputNameEl.value,
+        passWord:passWord.value,
+    }
+    if(fullNameEl.value.trim() === "" || !fullNameEl.value){
+        alert("Họ và tên không được để trống");
+        return;
+    }
+    if(inputNameEl.value.trim() === "" || !inputNameEl.value){
+        alert("Tên đằng nhập không được để trống");
+        return;
+    }else if(inputNameEl.value.length < 4){
+        alert("Tên đăng nhập phải có ít nhất 4 ký tự");
+        return;
+    }
+    if(passWord.value ===""){
+        alert("Mật khẩu không được để trống");
+        return;
+    }else if(passWord.value.length < 6){
+        alert("Mật khẩu tối thiểu phải là 6 ký tự");
+        return;
+    }
+    if(rePassWord.value !== passWord.value){
+        alert("Mật khẩu nhập lại không khớp");
+        return;
+    }
+    info.push(add);
+    localStorage.setItem("users", JSON.stringify(info));
+    fullNameEl.value = '';
+    passWord.value = '';
+    inputNameEl.value = '';
+    rePassWord.value = '';
+}
+btn.addEventListener("click" , addForm);
